3556df4cb2bdfb68fc5993d29fa8e596bf7f2005
EC2 Default User <ec2-user@ip-172-31-52-57.ec2.internal>
2021-11-28 03:41:03 +0000

    [books/show]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,15 @@ class UsersController < ApplicationController
 
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to books_path, flash: { notice: 'Welcome! You have signed up successfully.' }
+    else
+      render :new
+    end
+  end
+
   private
 
   def user_params
